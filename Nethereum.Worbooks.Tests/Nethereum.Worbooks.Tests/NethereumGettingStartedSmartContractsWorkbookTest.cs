4c57ba24e47951afc73c14f69067fad180fced36
Juan Blanco <juanfranblanco@hotmail.com>
2017-11-09 07:05:52 +0000

    Workbook unit testing

---
+++ Nethereum.Worbooks.Tests/Nethereum.Worbooks.Tests/NethereumGettingStartedSmartContractsWorkbookTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Xunit;
+
+namespace Nethereum.Worbooks.Tests
+{
+    public class NethereumGettingStartedSmartContractsWorkbookTest
+    {
+        private readonly MardownHelper _mardownHelper = new MardownHelper();
+
+        [Fact]
+        public async void Test()
+        {
+            var workbookText = System.IO.File.OpenText(workbook).ReadToEnd();
+            var code = _mardownHelper.ExtractCodeFromMarkdown(workbookText);
+
+            var state = await CSharpScript.RunAsync(code);
+            state = await state.ContinueWithAsync("return (balanceSecondAmountSend, originalBalanceFirstAmoundSend);");
+            var returnValue = (dynamic)state.ReturnValue;
+            Assert.Equal(2000, returnValue.Item1);
+            Assert.Equal(1000, returnValue.Item2);
+        }
+
+        private string workbook = @"nethereum-gettingstard-smartcontrats.workbook";
+       
+    }
+}
