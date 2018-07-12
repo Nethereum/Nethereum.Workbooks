429c551bfd19df8ea422093f417cab53fefbbb47
anegg0 <gael.blanchemain@protonmail.com>
2018-07-12 09:42:42 -0600

    refactoring (merge conflicts + file issues)

--- Nethereum.Worbooks.Tests/Nethereum.Worbooks.Tests/NethereumEventDtosGetAllChanges.cs
+++ Nethereum.Worbooks.Tests/Nethereum.Worbooks.Tests/NethereumEventDtosGetAllChanges.cs
@@ -18,17 +18,9 @@ namespace Nethereum.Worbooks.Tests
             //When
             var state = await CSharpScript.RunAsync(code);
             state = await state.ContinueWithAsync("return (gas, logs2);");
-<<<<<<< HEAD
             dynamic returnValue = (dynamic)state.ReturnValue;
             //Then
             Assert.Equal(25534444, returnValue.Item1.Value);
-=======
-            var returnValue = (dynamic)state.ReturnValue;
-            //Then
-            Assert.Equal(25535, returnValue.Item1.Value);
->>>>>>> f9fc970b817ce2fb25f4cd01648f35a84620bc40
-            Assert.Equal(1, returnValue.Item2.Count);
-
         }
     }
 }
