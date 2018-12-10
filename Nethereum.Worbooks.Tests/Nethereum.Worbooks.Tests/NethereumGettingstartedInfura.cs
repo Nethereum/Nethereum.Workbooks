d4639f82deb5b31ccd8e32a3f412b32d4df7f8bb
gael blanchemain <gael.blanchemain@consensys.net>
2018-12-10 14:19:58 -0500

    simple infura test

---
+++ Nethereum.Worbooks.Tests/Nethereum.Worbooks.Tests/NethereumGettingstartedInfura.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Nethereum.Hex.HexConvertors.Extensions;
+using Xunit;
+
+namespace Nethereum.Worbooks.Tests
+{
+    public class NethereumGettingStartedInfura : WorbookTest
+    {
+        public NethereumGettingStartedInfura() : base(WORKBOOK_PATH)
+        {
+        }
+
+        private const string WORKBOOK_PATH = "nethereum-gettingstarted-infura.workbook";
+
+        [Fact]
+        public async void Test()
+        {
+            var code = GetCodeSectionsFromWorkbook();
+            //When
+            var state = await CSharpScript.RunAsync(code);
+            state = await state.ContinueWithAsync("return etherAmount;");
+            Assert.NotNull(state.ReturnValue);
+            //Assert.Equal(40, state.ReturnValue.ToString().RemoveHexPrefix().Length);
+        }
+    }
+}
