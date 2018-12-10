46e2928364be79b7a24a0e740b0826902d4bff21
gael blanchemain <gael.blanchemain@consensys.net>
2018-12-10 16:42:01 -0500

    test on balance and mining

---
+++ Nethereum.Worbooks.Tests/Nethereum.Worbooks.Tests/NethereumGettingstartedTestchain.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Nethereum.Hex.HexConvertors.Extensions;
+using Xunit;
+
+namespace Nethereum.Worbooks.Tests
+{
+    public class NethereumGettingstartedTestchain : WorbookTest
+    {
+        public NethereumGettingstartedTestchain() : base(WORKBOOK_PATH)
+        {
+        }
+
+        private const string WORKBOOK_PATH = "nethereum-gettingstarted-testchain.workbook";
+
+        [Fact]
+        public async void Test()
+        {
+            var code = GetCodeSectionsFromWorkbook();
+            //When
+            var state = await CSharpScript.RunAsync(code);
+            state = await state.ContinueWithAsync("return (isMining, balance);");
+            var returnValue = (dynamic)state.ReturnValue;
+            Assert.True(returnValue.Item1); // override object.Equals
+            Assert.NotNull(returnValue.Item2);
+        }
+    }
+}
