14e406d0ec95af215b22e4434057d9fab1dbb0a0
anegg0 <gael.offers@protonmail.com>
2019-01-31 13:01:22 -0500

    Created post + test

---
+++ Nethereum.Worbooks.Tests/Nethereum.Worbooks.Tests/NethereumSigningTransactionsOfflineTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Nethereum.XUnitEthereumClients;
+using Xunit;
+
+namespace Nethereum.Worbooks.Tests
+{
+    [Collection(EthereumClientIntegrationFixture.ETHEREUM_CLIENT_COLLECTION_8)]
+    public class NethereumSigningTransactionsOfflineTest : WorbookTest
+    {
+        public NethereumSigningTransactionsOfflineTest() : base(WORKBOOK_PATH)
+        {
+        }
+
+        private const string WORKBOOK_PATH = "nethereum-signing-transactions-offline.workbook";
+
+        [Fact]
+        public async void Test()
+        {
+            var code = GetCodeSectionsFromWorkbook();
+            //When
+            var state = await CSharpScript.RunAsync(code);
+            state = await state.ContinueWithAsync("return (encoded, txId);");
+            dynamic returnValue = (dynamic)state.ReturnValue;
+            Assert.NotNull(returnValue.Item1);
+            Assert.Matches("^[0-9a-fA-F]{204}$", returnValue.Item1);
+            Assert.NotNull(returnValue.Item2);
+            Assert.Matches("^0x[0-9a-fA-F]{64}$", returnValue.Item2);
+        }
+    }
+}
