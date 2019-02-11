88e92eddccc4c1df48f58993d40bd2e0554eb6b3
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2019-02-11 17:31:50 -0500

    complete workbook with test

---
+++ Nethereum.Worbooks.Tests/Nethereum.Worbooks.Tests/NethereumManagingNoncesTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Nethereum.XUnitEthereumClients;
+using Xunit;
+
+namespace Nethereum.Worbooks.Tests
+{
+    [Collection(EthereumClientIntegrationFixture.ETHEREUM_CLIENT_COLLECTION_8)]
+    public class NethereumManagingNoncesTest : WorbookTest
+    {
+        public NethereumManagingNoncesTest() : base(WORKBOOK_PATH)
+        {
+        }
+
+        private const string WORKBOOK_PATH = "nethereum-managing-nonces.workbook";
+
+        [Fact]
+        public async void Test()
+        {
+            var code = GetCodeSectionsFromWorkbook();
+            //When
+            var state = await CSharpScript.RunAsync(code);
+            state = await state.ContinueWithAsync("return (transaction, txId);");
+            dynamic returnValue = (dynamic)state.ReturnValue;
+            //Then
+            Assert.NotNull(returnValue);
+            Assert.Matches("^0x[0-9a-fA-F]{64}$", returnValue.Item1);
+            Assert.Matches("^0x[0-9a-fA-F]{64}$", returnValue.Item2);
+        }
+    }
+}
