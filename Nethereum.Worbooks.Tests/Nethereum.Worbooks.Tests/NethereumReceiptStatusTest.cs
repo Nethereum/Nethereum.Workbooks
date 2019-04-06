ad383c3ada7f59c7bafee7f9c575dd3fb07b7510
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2019-04-05 20:00:53 -0400

    Added test + article file

---
+++ Nethereum.Worbooks.Tests/Nethereum.Worbooks.Tests/NethereumReceiptStatusTest.cs
@@ -0,0 +1,36 @@
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System;
+using System.Text;
+using Nethereum.XUnitEthereumClients;
+using Xunit;
+using System.Numerics;
+
+namespace Nethereum.Worbooks.Tests
+{
+    [Collection(EthereumClientIntegrationFixture.ETHEREUM_CLIENT_COLLECTION_17)]
+    public class NethereumReceiptStatusTest : WorbookTest
+    {
+        public NethereumReceiptStatusTest() : base(WORKBOOK_PATH)
+        {
+        }
+
+        private const string WORKBOOK_PATH = ".\\nethereum-receipt-status.workbook\\index.workbook";
+        private const string  PREFIXCODESECTION = ".\\nethereum-receipt-status.workbook\\receiptStatusData.csx";
+        [Fact]
+        public async void Test()
+        {
+            var prefixCode = LoadCodeSection(PREFIXCODESECTION);
+            var code = GetCodeSectionsFromWorkbook();
+            var usingsCode = ExtractUsingStatements(code);
+            var Rs = ExtractRStatements(code);
+            var usingsPrefix = ExtractUsingStatements(prefixCode);
+            prefixCode = RemoveLoadSections(prefixCode);
+            code = RemoveLoadSections(code);
+            var state = await CSharpScript.RunAsync(Rs + usingsCode+usingsPrefix +prefixCode+code);
+            state = await state.ContinueWithAsync("return (deploymentValidityStatus, transferValidityStatus);");
+            var returnValue = (dynamic)state.ReturnValue;
+            Assert.IsType<BigInteger>(returnValue.Item1);
+            Assert.IsType<BigInteger>(returnValue.Item2);
+        }
+    }
+}
